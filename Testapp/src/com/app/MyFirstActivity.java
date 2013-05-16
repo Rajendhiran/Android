fd48b67f8041ab023d13a8069884e601c27308f3
Rajendhiran .E <rajendhiran.mca@gmail.com>
2013-05-16 14:58:37 +0530

    TestApp
    Test

---
+++ Testapp/src/com/app/MyFirstActivity.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MyFirstActivity extends Activity 
+{
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_first);
+	}
+}
